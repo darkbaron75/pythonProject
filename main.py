e3c88c5e9d47009ac25afed2d010c79d3ec00470
ethan <18ecmille@malmesbury.wilts.sch.uk>
2021-08-17 14:04:10 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+colour = input("What is your favourite colour? ")
+if colour == "red":
+    print("Red is also my favourite colour")
+else:
+    print(colour + " is a nice colour but I prefer red myself")
+
+drink = input("Do you prefer cola or lemonade? ")
+if drink == "cola":
+    print("i like cola aswell")
+else:
+    print("its a nice drink but i prefer cola")
+
+team = input("What is your favourite football team? ")
+if team == "arsenal":
+    print("the thing with arsenal is they always walk it in ")
+else:
+    print("nice")
+
+colour = input("What is  your favourite colour? ")
+if colour == "red":
+    print("Red is my favourite colour as well")
+elif colour == "blue":
+    print("I like blue but I prefer red myself")
+elif colour == "green":
+    print("Green is a nice colour although I think that red is nicer")
+elif colour == "yellow":
+    print("Yellow is ok but it just isn't as great as red")
+elif colour == "pink":
+    print("Pink? I like pink but it isn't as nice as red")
+else:
+    print("I quite like that colour but I prefer red myself")
